4d1fa5470ee48de91e36e5c78f83b9bde7bfdf78
2015级喵班 <SchrodingerZhu>
2018-08-10 20:51:12 +0800

    add database operations

--- lib/test2/some_operations.ex
+++ lib/test2/some_operations.ex
@@ -1,6 +1,20 @@
 defmodule DCH do
-    @spec get_dch(string) :: string
+    import Ecto.Query
+    alias Test2.Repo
+    alias Test2.User
+
+    #Repo.insert(%User{name: "Jimmy", email: "jimmyzhu1999@protonmail.com"})
     def get_dch(s) do
         "DCH：" <> s
+    end
+    def get_jimmy_email() do
+        case Repo.all(from u in User, where: u.name == "Jimmy2" ,select: u.email) do
+            [] -> 
+                Repo.insert(%User{name: "Jimmy2", email: "jimmyzhu1999@protonmail.com"})
+                get_jimmy_email()
+            [email] -> 
+                email
+        end
+        
     end
 end
